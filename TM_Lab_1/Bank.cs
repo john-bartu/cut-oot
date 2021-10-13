49572e690bdf69efd0127f69431a0b0d17b7b70d
Jamabaiz <bartula.jan@gmail.com>
2021-10-13 13:38:18 +0200

    Code reformat

--- TM_Lab_1/Bank.cs
+++ TM_Lab_1/Bank.cs
@@ -1,14 +1,17 @@
 class Bank
 {
     float balance;
-    public Bank(){
-        balance=0;
+    public Bank()
+    {
+        balance = 0;
     }
-    public void Deposit(Account account){
+    public void Deposit(Account account)
+    {
         balance += account.Balance / account.Currency.Multiplier * account.Currency.AvgRate;
     }
 
-     public Account Balance(Currency currency){
-        return new Account( currency ,balance * currency.Multiplier / currency.AvgRate );
+    public Account Balance(Currency currency)
+    {
+        return new Account(currency, balance * currency.Multiplier / currency.AvgRate);
     }
 }
--- TM_Lab_1/Program.cs
+++ TM_Lab_1/Program.cs
@@ -2,7 +2,6 @@
 using System.Linq;
 using System.Xml.Linq;
 using System.Text;
-
 namespace TM_Lab_1
 {
     enum Command
