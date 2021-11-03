aed470287a02f1f34dbf274195177a728bc729d1
jamabaiz <jan@bartulacode.pl>
2021-11-03 11:16:57 +0100

    Lab3 Init

---
+++ TO_Lab_3/src/pl/retsuz/shell/variations/mkdir/Mkdir_Path.java
@@ -0,0 +1,32 @@
+package pl.retsuz.shell.variations.mkdir;
+
+import pl.retsuz.filesystem.Composite;
+import pl.retsuz.filesystem.IComposite;
+import pl.retsuz.shell.gen.ICommand;
+import pl.retsuz.shell.variations.gen.CommandVariation;
+import pl.retsuz.shell.variations.gen.ICommandVariation;
+
+public class Mkdir_Path extends CommandVariation {
+    public Mkdir_Path(ICommandVariation next, ICommand parent) {
+        super(next,parent,"[a-zA-Z0-9.l\\/_]*");
+    }
+    @Override
+    public void make(String params) {
+
+        Composite c= (Composite) (this.getParent().getContext().getCurrent());
+
+
+
+        try {
+            System.out.println(params);
+
+            IComposite elem = c.findElementByPath(params);
+            c.addElement(elem);
+
+        }catch(Exception e){
+            System.out.println("Docelowy element nie jest katalogiem lub obecny katalog nie zawiera elementu.");
+            System.out.println(e.getMessage());
+        }
+
+    }
+}
