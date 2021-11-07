4d479a8c50b6f8e53b780f90f42a884f4101c004
john-bartu <jan@bartulacode.pl>
2021-11-07 17:18:28 +0100

    Basic functionality

--- NutrientsProject/Program.cs
+++ NutrientsProject/Program.cs
@@ -1,7 +1,15 @@
+using System;
+using System.Configuration;
+using System.Linq;
+using System.Text.Json;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using NutrientsProject.Source;
+using Pomelo.EntityFrameworkCore.MySql;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,21 +20,41 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+Console.WriteLine(builder.Configuration.GetSection("ConnectionStrings")["MysqlDatabase"]);
+
+// Add service of database
+var connectionString = builder.Configuration.GetConnectionString("MysqlDatabase");
+// var serverVersion = new MySqlServerVersion(connectionString);
+var serverVersion = new MySqlServerVersion(new MySqlServerVersion("5.7.35"));
+builder.Services.AddDbContext<DatabaseContext>(dbContextOptions =>
+    dbContextOptions.UseMySql(connectionString, serverVersion));
+
+// var connectionString = builder.Configuration.GetConnectionString("PostgressDatabase");
+// builder.Services.AddDbContext<DatabaseContext>(dbContextOptions =>
+//     dbContextOptions.UseNpgsql(connectionString));
+
+builder.Services.AddCors();
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(
+        c =>
+        {
+            c.DisplayRequestDuration();
+            c.InjectStylesheet("/dark.css");
+        }
+    );
 }
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
+app.UseStaticFiles();
 app.MapControllers();
-
 app.Run();
-
-Database.DefaultData();
