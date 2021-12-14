735065957286c4445f10ee44bd5df9a82eb119c5
john-bartu <jan@bartulacode.pl>
2021-12-14 21:38:55 +0100

    Main program with generator

---
+++ TO_Lab_6/Program.cs
@@ -0,0 +1,39 @@
+﻿using TO_Lab_6;
+
+Console.WriteLine("Hello, World!");
+
+RegistryOffice registryOffice = new RegistryOffice();
+
+
+string[] names =
+    { "Jan", "Marcin", "Filip", "Szymon", "OskAr", "Artem", "Karol", "Eugeniusz", "Tomasz", "Karol" };
+string[] surnames =
+    { "Jarosz", "Rutecki", "Kocjan", "Waligóra", "Marcol", "Boczar", "Zubek", "Bojanowski", "Szcześniak", "Łęczycki" };
+
+
+string RandomName()
+{
+    List<string> parts = new();
+    for (int i = 0; i <= Random.Shared.Next(3); i++)
+        parts.Add(names[Random.Shared.Next(names.Length)]);
+
+    for (int i = 0; i <= Random.Shared.Next(3); i++)
+        parts.Add(surnames[Random.Shared.Next(surnames.Length)]);
+
+    return String.Join(" ", parts);
+}
+
+
+for (int i = 0; i <= 10; i++)
+{
+    registryOffice.RegisterPerson(RandomName(), Person.RandomLocation());
+}
+
+registryOffice.ExportMemoryTree();
+
+registryOffice.ListWithMemory();
+registryOffice.Export("export.json");
+
+RegistryOffice secondRegistryOffice = RegistryOffice.Import("export.json");
+secondRegistryOffice.ListWithMemory();
+secondRegistryOffice.Export("export2.json");
